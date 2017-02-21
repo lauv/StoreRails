284edb8ec8ef66c0dda71922f7b90536e67ac524
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-21 16:47:53 -0300

    item 7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
 	belongs_to :category
 	validates :price, presence: true
-	before_validate : set_premium
+	before_validation :set_premium
 	def set_premium
 		self.premium ||= false # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Category.destroy_all
+
+3.times do |t|
+	Category.create(name: "categoria #{t}")
+end
+
+Product.create(name: "Producto 1", category_id: Category.find_by(name: "categoria 0").id, price: 300)
+categoria = Category.find_by(name: "categoria 0")
+producto = categoria.products.build(name: "Producto_build 1", price: 300)
+producto.save
+
+b = Product.last
+b.destroy
+
+c = Product.last
+c.premium = true
+c.save
+
+categorias = Category.all
+categorias.each do |cat|
+	cat.name.upcase!
+	cat.save
+end
