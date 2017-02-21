20f17808896463732cd32fd9d9820eb19cfcab95
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-21 17:24:48 -0300

    item 3 backend avanzado

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
 	belongs_to :category
 	validates :price, presence: true
 	before_validation :set_premium
+	
+	scope :premium_n, -> {where(premium: true)}
+	scope :last_n, ->(cant) {order('id DESC').limit(cant)}
 	def set_premium
 		self.premium ||= false # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
 	end
