7b9e9a4fa6f4c7af31bab26a12b7686f73cdf4c9
Laura Díaz Vera <valentina.hp@gmail.com>
2017-02-21 14:43:07 -0300

    Item 6

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
-  belongs_to :category
+	belongs_to :category
+	validates :price, presence: true
+	before_validate : set_premium
+	def set_premium
+		self.premium ||= false # note self.status = 'P' if self.status.nil? might be safer (per @frontendbeauty)
+	end
 end
